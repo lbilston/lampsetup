ee1ed82dae1119897016c955503c9741070b64d5
Luke <lbilston@users.noreply.github.com>
2014-11-27 13:13:54 +1100

    Update lampsetup.sh

--- lampsetup.sh
+++ lampsetup.sh
@@ -3,10 +3,14 @@
 # Script by Luke Bilston
 # Free to steal, modify and claim as your own
 
+# update should be done as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
 
 # Proxy Support
-export http_proxy = http://10.130.48.19:8080
-
+export http_proxy=http://10.130.48.19:8080
 
 # Required repository for webmin
 echo deb http://download.webmin.com/download/repository sarge contrib >> /etc/apt/sources.list
