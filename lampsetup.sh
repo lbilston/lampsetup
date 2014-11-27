b62599896f2b0856f0f60b6f8dbda417a448c4e1
Luke <lbilston@users.noreply.github.com>
2014-11-27 13:11:27 +1100

    Create lampsetup.sh

---
+++ lampsetup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Script by Luke Bilston
+# Free to steal, modify and claim as your own
+
+
+# Proxy Support
+export http_proxy = http://10.130.48.19:8080
+
+
+# Required repository for webmin
+echo deb http://download.webmin.com/download/repository sarge contrib >> /etc/apt/sources.list
+echo deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib /etc/apt/sources.list
+
+# Keys for Webmin
+cd ~
+wget http://www.webmin.com/jcameron-key.asc
+apt-key add jcameron-key.asc
+
+# Update to latest Version
+apt-get update -y
+apt-get dist-upgrade -y
+
+# Time to Install
+apt-get install -y ssh
+apt-get install -y apache2 php5 libapache2-mod-php5 
+apt-get install -y mysql-server mysql-client php5-mysql
+apt-get install -y phpmyadmin
+apt-get install -y webmin
